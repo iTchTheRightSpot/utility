7d2ce366cb65ee3fea1e8f407a07010f9f297548
iTchTheRightSpot <emmanueluluabuike@gmail.com>
2025-04-14 19:13:37 -0400

    added transaction logic

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,10 @@
 package utils
 
-import "time"
+import (
+	"context"
+	"database/sql"
+	"time"
+)
 
 type RequestContextKey string
 
@@ -22,4 +26,38 @@ func Timezone(timezone string) (*time.Location, error) {
 		return nil, err
 	}
 	return location, nil
+}
+
+func RunInTx(ctx context.Context, l ILogger, db *sql.DB, fn func(*sql.Tx) error) error {
+	l.Log(ctx, "STARTING TRANSACTION")
+
+	tx, err := db.Begin()
+	if err != nil {
+		l.Critical(ctx, "FAILED TO BEGIN TRANSACTION: "+err.Error())
+		return &ServerError{}
+	}
+
+	err = fn(tx)
+	if err == nil {
+		l.Log(ctx, "COMMITTING TRANSACTION")
+
+		if commitErr := tx.Commit(); commitErr != nil {
+			l.Critical(ctx, "FAILED TO COMMIT TRANSACTION: "+commitErr.Error())
+			return &ServerError{}
+		}
+
+		l.Log(ctx, "TRANSACTION COMMITTED SUCCESSFULLY")
+		return nil
+	}
+
+	l.Error(ctx, "TRANSACTION FUNCTION RETURNED ERROR: "+err.Error())
+	l.Log(ctx, "ROLLING BACK TRANSACTION")
+
+	if rollbackErr := tx.Rollback(); rollbackErr != nil {
+		l.Critical(ctx, "FAILED TO ROLLBACK TRANSACTION: "+rollbackErr.Error())
+		return &ServerError{}
+	}
+
+	l.Log(ctx, "TRANSACTION ROLLED BACK SUCCESSFULLY")
+	return err
 }
